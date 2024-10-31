ba36cf6033cede8a1ff09325b662e586c8120bbe
Rasibul <rasibul179@gmail.com>
2024-10-31 21:46:36 +0600

    data type in typescript

---
+++ module1/src/1.4.ts
@@ -0,0 +1,44 @@
+// basic data types
+{
+    // primitive data types
+
+
+    //string
+    let course: string = "TypeScript";
+
+    //number
+    let age: number = 30;
+
+    //boolean
+    let isLoggedIn: boolean = true;
+
+    //null
+
+    let y: null = null
+
+    //undefined
+
+    let z: undefined = undefined
+
+    //any
+    let x: any = "hello";
+
+
+    // non primitive data types
+    
+
+    //array
+
+    let friends: string[] = ["sam", "james", "john"]
+    let numbers: number[] = [1, 2, 3, 4, 5]
+    let truths: boolean[] = [true, false, true]
+
+    // tuple --> array--> order-->
+
+    let ageNumber :[number,string] = [50,"Rasibul"]
+
+    ageNumber[0] = 70
+
+
+
+}
